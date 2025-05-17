96fb9b34ea16a30db575245b13c467afd6acae4b
Ahmed Moniem <ahmedabdalmonem121@gmail.com>
2025-05-17 23:17:26 +0300

    working Cart

--- itiproject/cart/urls.py
+++ itiproject/cart/urls.py
@@ -5,4 +5,8 @@ from .views import CartItemViewSet
 router = DefaultRouter()
 router.register(r'cart', CartItemViewSet, basename='cart')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+     path('cart/', CartItemViewSet.as_view({'get': 'list', 'post': 'create'}), name='cart-list'),
+     path('cart/<int:pk>/', CartItemViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='cart-detail'),
+]
