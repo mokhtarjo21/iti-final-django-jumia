fd2dc88fcdbb56d84c59ed3f96bf5dd6c0202d2e
mokhtarjo21 <mokhtar.jo21@gmail.com>
2025-05-23 06:59:54 +0300

    check customer buy product or not

--- itiproject/orders/urls.py
+++ itiproject/orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CheckoutView, VendorOrderItemsView 
-from .views import PaymobPaymentView
+from .views import *
+
 
 
 urlpatterns = [
@@ -8,6 +8,6 @@ urlpatterns = [
     path("vendor-items/", VendorOrderItemsView.as_view()),
     path("vendor-items/<int:pk>/", VendorOrderItemsView.as_view()),  # PATCH endpoint
     path('paymob/', PaymobPaymentView.as_view(), name='paymob-payment'),
-    
+    path('check/<slug:product_id>/',UserOrder.as_view(),name='check-order')
 
 ]
